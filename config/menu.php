0a5c1d09cac1e8f144868ed261b26e0b38bb08cb
陈李俊 <595572594@qq.com>
2020-03-22 11:12:34 +0800

    url调整;

--- config/menu.php
+++ config/menu.php
@@ -3,66 +3,66 @@
 return [
     [
         'name' => '欢迎页',
-        'url' => 'http://www.baidu.com',
+        'url' => '/admin/home',
         'icon' => 'layui-icon-chart-screen'
     ],
     [
         'name' => '首页管理',
-        'url' => 'http://www.baidu.com',
+        'url' => '/admin/index',
         'icon' => 'layui-icon-table',
         'children' => [
             [
                 'name' => '轮播图',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/index/carousel',
                 'icon' => 'layui-icon-carousel',
             ],
             [
                 'name' => '导航栏',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/index/nav',
                 'icon' => 'layui-icon-picture',
             ],
             [
                 'name' => '公告',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/index/news',
                 'icon' => 'layui-icon-read',
             ],
         ]
     ],
     [
         'name' => '用户与角色',
-        'url' => 'http://www.baidu.com',
+        'url' => '/admin/user_management',
         'icon' => 'layui-icon-user',
         'children' => [
             [
                 'name' => '用户管理',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/user_management/user',
                 'icon' => 'layui-icon-friends',
             ],
             [
                 'name' => '角色管理',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/user_management/role',
                 'icon' => 'layui-icon-group',
             ]
         ]
     ],
     [
         'name' => '系统设置',
-        'url' => 'http://www.baidu.com',
+        'url' => '/admin/config',
         'icon' => 'layui-icon-set',
         'children' => [
             [
                 'name' => '基础设置',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/config/base',
                 'icon' => 'layui-icon-set',
             ],
             [
                 'name' => '菜单设置',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/config/menu',
                 'icon' => 'layui-icon-set',
             ],
             [
                 'name' => '相册管理',
-                'url' => 'http://www.baidu.com',
+                'url' => '/admin/config/album',
                 'icon' => 'layui-icon-picture',
             ],
         ]
